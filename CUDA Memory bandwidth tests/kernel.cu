c92feead9bc1dfbfd4f75b65cbcb99065a1aced1
Saitama10000 <62643420+Saitama10000@users.noreply.github.com>
2020-05-31 19:23:55 +0300

    edits again

--- CUDA Memory bandwidth tests/kernel.cu
+++ CUDA Memory bandwidth tests/kernel.cu
@@ -1,29 +1,13 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "error.h"
+#include "kernel.ptx"
 #include <cuda.h>
 #include <time.h>
 #include <stdio.h>
 
-extern "C" __global__ void getClock(int* timings)
-{
-	int tid = threadIdx.x;
-	unsigned long long start = clock64();
-
-	unsigned long long end = clock64();
-	timings[tid] = start;
-	timings[tid + 1] = end;
-}
-
 int main()
 {
-	const long long size = 1ll << 28;
-	int* d_input;
-	int* d_output;
-	cudaErr(cudaMalloc((void**)&d_input, size * sizeof(int)));
-	cudaErr(cudaMalloc((void**)&d_output, size * sizeof(int)));
-
-
 	CUdevice device;
 	CUcontext context;
 	CUmodule module;
@@ -33,39 +17,36 @@ int main()
 	cuErr(cuInit(0));
 	cuErr(cuDeviceGet(&device, 0));
 	cuErr(cuCtxCreate(&context, 0, device));
-	cuErr(cuModuleLoad(&module, "kernel.ptx"));
+	cuErr(cuModuleLoadData(&module, kernel_ptx));
 	cuErr(cuModuleGetFunction(&get_clock, module, "get_clock"));
 	cuErr(cuModuleGetFunction(&get_time, module, "get_time"));
 
-	unsigned long long h_clock[2];
+	unsigned long long h_clock[1];
+	unsigned long long clock = 0;
+	unsigned long long time = 0;
+	unsigned long long n = 0;
+	
 	CUdeviceptr d_clock;
-	cuErr(cuMemAlloc(&d_clock, 2 * sizeof(h_clock[0])));
-
-	for (int i = 0; i < 16; i++)
+	cuErr(cuMemAlloc(&d_clock, sizeof(h_clock[0])));
+	
+	while(true)
 	{
 		{
 			void* args[] = { &d_clock };
 			cuErr(cuLaunchKernel(get_clock, 1, 1, 1, 1, 1, 1, 0, NULL, (void**)args, NULL));
-			cuErr(cuMemcpyDtoH((void*)&h_clock, d_clock, 2 * sizeof(h_clock[0])));
-			printf("Clocks: %lld %lld %lld\n", h_clock[0], h_clock[1], h_clock[1] - h_clock[0]);
+			cuErr(cuMemcpyDtoH((void*)&h_clock, d_clock, sizeof(h_clock[0])));
+			clock += h_clock[0];
 		}
 		{
 			void* args[] = { &d_clock };
 			cuErr(cuLaunchKernel(get_time, 1, 1, 1, 1, 1, 1, 0, NULL, (void**)args, NULL));
-			cuErr(cuMemcpyDtoH((void*)&h_clock, d_clock, 2 * sizeof(h_clock[0])));
-			printf("Time: %lld %lld %lld\n", h_clock[0], h_clock[1], h_clock[1] - h_clock[0]);
+			cuErr(cuMemcpyDtoH((void*)&h_clock, d_clock, sizeof(h_clock[0])));
+			time += h_clock[0];
 		}
-		{
-			getClock<<<1, 1>>>((void*)d_clock);
-			cudaErr(cudaMemcpy((void*)&h_clock, (void*)d_clock, 2 * sizeof(h_clock[0]), cudaMemcpyDeviceToHost));
-			printf("Time: %lld %lld %lld\n", h_clock[0], h_clock[1], h_clock[1] - h_clock[0]);
-		}
+		n += 1;
+		printf("\rClock: %8.2f Time: %8.2f", (double)(clock) / n, (double)(time) / n);
 	}
 
-
-
-	cudaErr(cudaFree(d_input));
-	cudaErr(cudaFree(d_output));
-	cudaDeviceReset();
+	cudaErr(cudaDeviceReset());
 	return 0;
 }
